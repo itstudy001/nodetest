b3a6b730230fa83ba74efe770ff8c4e84b932af0
itstudy001 <itstudy@kakao.com>
2023-10-16 23:45:07 +0900

    message

--- test.spec.js
+++ test.spec.js
@@ -1,9 +1,9 @@
 describe('Default Test Set', () => {
     it('test1 should be passed', () => {
-        console.log('test1 passed')
+        console.log('test1 성공')
     })
 
     it('test2 should be passed', () => {
-        console.log('test2 passed')
+        console.log('test2 성공')
     })
 })
