47dc3796806c9a7fe8d028dab740fd2d19157680
itstudy <72733143+itstudy001@users.noreply.github.com>
2023-10-17 12:18:23 +0900

    Update test.spec.js

--- test.spec.js
+++ test.spec.js
@@ -1,9 +1,9 @@
 describe('Default Test Set', () => {
     it('test1 should be passed', () => {
-        console.log('test1 성공')
+        console.log('test1 success')
     })
 
     it('test2 should be passed', () => {
-        console.log('test2 성공')
+        console.log('test2 success')
     })
 })
